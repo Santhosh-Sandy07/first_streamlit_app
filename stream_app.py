cb4accf5292a950aaca0fcdb50f5fef18054af5d
Santhosh-Sandy07 <155747273+Santhosh-Sandy07@users.noreply.github.com>
2024-01-10 20:39:23 +0530

    Update stream_app.py

--- stream_app.py
+++ stream_app.py
@@ -20,7 +20,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 fruit_choice=streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write=('The user entered',fruit_choice)
+streamlit.write('The user entered',fruit_choice)
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
@@ -37,4 +37,4 @@ streamlit.text(my_data_rows)
 streamlit.dataframe(my_data_rows)
 
 add_fruit=streamlit.text_input('What fruit would you like to add?', 'Jackfruit')
-streamlit.write=('Thanks for adding',add_fruit)
+streamlit.write('Thanks for adding',add_fruit)
